9c568160e014e4e9e34b1471d0948d8886645b60
Eviee <evesaysmeow13@gmail.com>
2023-09-22 15:21:31 +1200

    Map test

--- music_starter.js
+++ music_starter.js
@@ -9,19 +9,24 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    let bar_spacing = height / 10;
    let bar_height = width / 12;
    let bar_pos_x = width / 2;
- 
+   let ballSize = 40;
+   let drumHeight = map(drum, 0, 100, 0+ballSize/2, height);
+
+   
+   fill(0, 200, 0);
+   ellipse(width/2, drumHeight, ballSize);
 
    // vocal bar is red
-   fill(200, 0, 0);
-   rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
-   fill(0);
-   text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
+  //  fill(200, 0, 0);
+  //  rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
+  //  fill(0);
+  //  text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
  
    // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
+  //  fill(0, 200, 0);
+  //  rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
+  //  fill(0);
+  //  text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
  
    // bass bar is blue
    fill(50, 50, 240);
