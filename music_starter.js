ad5e89b6429967a44bc1c32a9a81da4d9967ba8a
Eviee <evesaysmeow13@gmail.com>
2023-09-28 10:02:37 +1300

    28/09

--- music_starter.js
+++ music_starter.js
@@ -1,9 +1,16 @@
-
-
+let firstRun = true
+let frame;
+let vol;
 
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
-  background(20)
+  if(firstRun){
+    rectMode(CENTER);
+    frame = loadImage('tvframe.png');
+    vol = loadImage('volume.png');
+    firstRun = false;
+  }
+  background(0, 98, 255)
   textFont('Helvetica'); // please use CSS safe fonts
   rectMode(CENTER)
   textSize(24);
@@ -13,8 +20,6 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    let bar_pos_x = width / 2;
    let ballSize = 40;
    let vocalHeight = map(vocal, 0, 100, 0+ballSize/2, height);
-   
-   let particles = [];
 
    fill(255);
    ellipse(width/2, vocalHeight, ballSize);
@@ -24,47 +29,17 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    rect(bar_pos_x, height / 2 + 1 * bar_spacing, 4 * vocal, bar_height);
    fill(0);
    text("vocals", bar_pos_x, height / 2 + 1 * bar_spacing + 8);
- 
-   // drum bar is green
-   fill(0, 200, 0);
-   rect(bar_pos_x, height / 2 + 2 * bar_spacing, 4 * drum, bar_height);
-   fill(0);
-   text("drums", bar_pos_x, height / 2 + 2 * bar_spacing + 8);
- 
-   // bass bar is blue
-   fill(50, 50, 240);
-   rect(bar_pos_x, height / 2 + 3 * bar_spacing, 4 * bass, bar_height);
-   fill(0);
-   text("bass", bar_pos_x, height / 2 + 3 * bar_spacing + 8);
- 
-  //  // other bar is white
-   fill(200, 200, 200);
-   rect(bar_pos_x, height / 2 + 4 * bar_spacing, 4 * other, bar_height);
-   fill(0);
-   text("other", bar_pos_x, height / 2 + 4 * bar_spacing + 8);
-   fill(255, 255, 0);
- 
-   // display "words"
-   textAlign(CENTER);
-   textSize(vocal);
-   text(words, width/2, height/3);
 
+  image(frame, 0, 0);
+
+    //volume bar
+    let volheight = 20;
+    let volwidth = 70;
+    let volX = 480;
+    let volY = 880;
+    image(vol, 450, 750);
+    vol.resize(200,100);
+    noStroke();
+    fill(255);
+    rect(volX, volY, volheight, volwidth);
 }
-
-
-  	//Display the particle effects and detects the beat of the audio
-    push();
-  	translate(width/2,height/2);
-  	shadow();
-    let p = new Particle();
-    particles.push(p);
-    
-    for(let i = 0; i < particles.length; i++) {
-      if(!particles[i].edges()) {
-      particles[i].update(amp > 230);
-      particles[i].show();
-      } else {
-        particles.splice(i, 1)
-      }
-    }
-  pop();
