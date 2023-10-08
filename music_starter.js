bb1daaac6b9565b4f81ad4a467264e4c8f1649d0
Eviee <evesaysmeow13@gmail.com>
2023-10-09 10:19:09 +1300

    asset update

--- music_starter.js
+++ music_starter.js
@@ -1,13 +1,14 @@
 let firstRun = true
 let frame;
 let vol;
-
+let errortab;
 // vocal, drum, bass, and other are volumes ranging from 0 to 100
 function draw_one_frame(words, vocal, drum, bass, other, counter) {
   if(firstRun){
     rectMode(CENTER);
     frame = loadImage('assets/tvframe.png');
     vol = loadImage('assets/volume.png');
+    errortab = loadImage('assets/errortest.png')
     firstRun = false;
   }
   background(23, 0, 92) //blue screen 0, 98, 255
@@ -21,14 +22,27 @@ function draw_one_frame(words, vocal, drum, bass, other, counter) {
    let ballSize = 40;
    let vocalHeight = map(vocal, 0, 100, 0+ballSize/2, height);
 
-  image(frame, 0, 0);
+
 
   console.log(counter);
-  if(counter>=2860 && counter<=4300){
+  if(counter>=2860 && counter<=4300 || 
+    counter>=7200 && counter<=9300){
     volumebar (vocal);
   }
 
+  if(counter>=7140 && counter<=7190){
+    background(0, 98, 255);
+  }
 
+  if(counter>=7140 && counter<=7190){
+  
+  }
+
+  if(counter>=2860 && counter<=4300){
+  errortabs();
+  }
+
+  image(frame, 0, 0);
 }
 
 function volumebar(vocal){
@@ -43,7 +57,13 @@ function volumebar(vocal){
   fill(255);
 
 for(let i =1; i <= vocalvol; i++){
-  let barstep = i*30;
+  let barstep = i*30; 
   rect(barstep+450, volY, volheight, volwidth);
 }
+}
+
+function errortabs(){
+  errortab.resize(800,450);
+  image(errortab, 0, 0);
+
 }
